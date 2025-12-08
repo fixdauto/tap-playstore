6bacf2db4f3c7c28639c8f80b61ff5b7b587608c
Bacotell36 <ryan@fixdapp.com>
2025-12-08 16:30:45 -0500

    add new required package

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     install_requires=[
         'singer-python>=5.12.1',
         'tap-gcs-csv @ git+https://github.com/fixdauto/tap-gcs-csv.git@v0.0.3',
+        'importlib-metadata>=5.0.0',
     ],
     entry_points="""
     [console_scripts]
